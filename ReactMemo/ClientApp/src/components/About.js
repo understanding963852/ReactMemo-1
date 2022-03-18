16d561630351b70c5e501b0a4b50356ddff9e883
understanding963952 <suuneeee@naver.com>
2022-03-18 18:52:27 +0900

    초기 커밋.

---
+++ ReactMemo/ClientApp/src/components/About.js
@@ -0,0 +1,13 @@
+﻿import React, { Component } from 'react';//react에서 참조를 걸어준다
+
+
+
+export class About extends Component {//Component클래스를 상속한 About 클래스를 export시킨다.
+    render() {
+        return (
+            <div>
+                  <h1>정보</h1>
+            </div>
+            )
+    }
+}
---
+++ ReactMemo/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ReactMemo.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
