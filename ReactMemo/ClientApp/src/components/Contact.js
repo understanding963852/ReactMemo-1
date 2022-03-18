e418fbf58b780c35f41831d7204d6ace56914c99
understanding963952 <suuneeee@naver.com>
2022-03-19 01:39:49 +0900

    ㄹㄹㄹ

---
+++ ReactMemo/ClientApp/src/components/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+
+export class Contact extends Component {
+    render() {
+
+        return (
+            <div>
+                <h1>연락처 페이지</h1>
+                <p>연락처 들어오는곳</p>
+            </div>
+        )
+    }
+
+}
